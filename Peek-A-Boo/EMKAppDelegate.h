86ec2dcb504146f7e71fa300e830a20daf9268ba
Erik <mail@kalaxy.net>
2013-06-15 20:27:06 +0200

    Initial Commit

---
+++ Peek-A-Boo/EMKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EMKAppDelegate.h
+//  Peek-A-Boo
+//
+//  Created by Erik on 15.06.13.
+//  Copyright (c) 2013 Kalaxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Peek-A-Boo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Peek-A-Boo
+//
+//  Created by Erik on 15.06.13.
+//  Copyright (c) 2013 Kalaxy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EMKAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EMKAppDelegate class]));
+    }
+}
